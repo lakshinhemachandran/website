d6edd0d3f4f652e7bbf6dbf21e6cc59ba9d6eb6f
Lakshin Hemachandran <hlakshin@gmail.com>
2025-08-30 15:47:47 -0400

    Polish contact form styles and scrolling fixes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import Navbar from '../components/Navbar/Navbar'
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import "./Home.css";
 
-export default function Contact () {
-    return (
-        <>
-            <Navbar />
-        </>
-    )
+export default function Contact() {
+  return (
+    <>
+      <Navbar />
+      <div className="contact-page-wrapper">
+        <main className="contact-container">
+          <h1>Contact Me</h1>
+          <form action="https://formsubmit.co/hlakshin@gmail.com" method="POST" className="contact-form">
+            <input type="text" name="name" placeholder="Your Name" required />
+            <input type="email" name="email" placeholder="Your Email" required />
+            <textarea name="message" placeholder="Your Message" required rows={5} />
+            <button type="submit">Send</button>
+          </form>
+        </main>
+      </div>
+    </>
+  );
 }
