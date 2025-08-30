3dc400e1c1c1e9d0a338caed17c4dbe413172c92
Lakshin Hemachandran <hlakshin@gmail.com>
2025-08-30 16:43:02 -0400

    update form on about me page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -9,7 +9,14 @@ export default function Contact() {
       <div className="contact-page-wrapper">
         <main className="contact-container">
           <h1>Contact Me</h1>
-          <form action="https://formsubmit.co/hlakshin@gmail.com" method="POST" className="contact-form">
+          <form 
+            action="https://formsubmit.co/hlakshin@gmail.com" 
+            method="POST" 
+            className="contact-form"
+          >
+            <input type="hidden" name="_captcha" value="false" />
+            <input type="hidden" name="_next" value="https://quantumdev.cc" />
+
             <input type="text" name="name" placeholder="Your Name" required />
             <input type="email" name="email" placeholder="Your Email" required />
             <textarea name="message" placeholder="Your Message" required rows={5} />
