b80602dcdecfff1e2adc0f96c9a3f6b6202e12d5
Lakshin Hemachandran <hlakshin@gmail.com>
2026-03-20 16:20:12 -0400

    Update projects in Projects.jsx
    Replaced 'Pokemon Finder' project with 'GradeCalc' project and removed 'Todo List App'.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -15,23 +15,16 @@ export default function Projects () {
       github="https://github.com/lakshinhemachandran/website"
     />
     <ProjectCard
-      title="Pokemon Finder"
-      description="A simple project where you can input the name of a Pokemon and get a picture of it in return."
-      url="https://pokemon-finder.lakshin.dev"
-      github="https://github.com/lakshinhemachandran/Pokemon-Finder"
+      title="GradeCalc"
+      description="Calculate your projected average by combining your current gradebook with pending assignments."
+      url="https://gradecalc.lakshin.dev"
+      github="https://github.com/lakshinhemachandran/gradecalc"
     />
     <ProjectCard
       title="Weather App"
       description="A weather app built with JavaScript that uses the VisualCrossing Weather API. There is no live demo yet due to API key limitations."
       github="https://github.com/lakshinhemachandran/WeatherApp"
     /> 
-    <ProjectCard
-      title="Todo List App"
-      description="A simple todo list app built with React.js"
-      url="https://todo-list.lakshin.dev"
-      github="https://github.com/lakshinhemachandran/ReactTodoListApp"
-    
-    />  
       <ProjectCard
       title="Racelink"
       description="WORK IN PROGRESS - Racelink is a React web app for exploring real-time and historical F1 data via the OpenF1 API. It offers race results, driver/team profiles, lap-time charts, and session info. A smart search maps common names to API IDs, while a responsive UI and reusable components highlight API integration and data visualization."
